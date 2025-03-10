26d803caf4089f5e4561402462b15424674e03ce
Sonal Choudhary <sonalchoudhary2804@gmail.com>
2025-03-11 00:23:15 +0530

    episode 1

---
+++ index.js
@@ -0,0 +1,40 @@
+//  const h1 = React.createElement('h1', {}, 'Hello Sonal!! Learn React');
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(h1);
+
+
+// <div id="parent">
+//     <div id="child">
+//         <h1>Hello....</h1>
+//     </div>
+//      <div id="child2">
+//         <h1>Hello....</h1>
+//     </div>
+// </div>
+
+
+const parent = React.createElement(
+    'div', 
+    {id:"parent"}, 
+    [React.createElement(
+        'div', 
+        {id:"child1"}, 
+        React.createElement(
+            'h1', 
+            {}, 
+            "Child1"
+        )
+    ),
+    React.createElement(
+        'div', 
+        {id:"child2"}, 
+        React.createElement(
+            'h1', 
+            {}, 
+            "Child2"
+        )
+    )]
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
